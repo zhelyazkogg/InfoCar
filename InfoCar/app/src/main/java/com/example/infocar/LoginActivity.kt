b5c8639a46de3382646cabe4302f2124b95099ac
unknown <zhelyazkogg@gmail.com>
2020-02-21 17:22:29 +0200

    declaring the atributes in loginActivity

--- InfoCar/app/src/main/java/com/example/infocar/LoginActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/LoginActivity.kt
@@ -2,13 +2,22 @@ package com.example.infocar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class LoginActivity : AppCompatActivity() {
 
-    
+    lateinit var emailLogin : EditText
+    lateinit var passwordLogin : EditText
+    lateinit var loginBtn : Button
+    lateinit var registerBtn : Button
+    lateinit var forgottenPass : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        
     }
 }
--- InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
@@ -2,8 +2,11 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 
-class ProfileActivity : AppCompatActivity() {
+    class ProfileActivity : AppCompatActivity() {
+
+    lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
