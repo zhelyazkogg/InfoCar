83e8ae6961b4db21c1f5613b53c1871df8416713
unknown <zhelyazkogg@gmail.com>
2020-03-02 18:10:58 +0200

    removed vehicle year

--- InfoCar/app/src/main/java/com/example/infocar/AddCarActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/AddCarActivity.kt
@@ -10,7 +10,7 @@ class AddCarActivity : AppCompatActivity() {
 
     lateinit var brandRegister : EditText
     lateinit var modelRegister : EditText
-    lateinit var vehicleYearRegister : EditText
+
     lateinit var hpRegister : EditText
     lateinit var yoursSinceRegister : EditText
     lateinit var kmRegister : EditText
@@ -31,7 +31,7 @@ class AddCarActivity : AppCompatActivity() {
 
         brandRegister = findViewById(R.id.brandRegister)
         modelRegister = findViewById(R.id.modelRegister)
-        vehicleYearRegister = findViewById(R.id.vehicleYearRegister)
+
         hpRegister = findViewById(R.id.hpRegister)
         yoursSinceRegister = findViewById(R.id.yoursSinceRegister)
         kmRegister = findViewById(R.id.kmRegister)
