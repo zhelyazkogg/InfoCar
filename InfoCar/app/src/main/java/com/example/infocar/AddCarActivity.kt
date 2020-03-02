9088a6d2c801cceb826470df5e7bd3d07969c30a
unknown <zhelyazkogg@gmail.com>
2020-03-02 14:28:16 +0200

    validating the EditText fields in AddCarActivity

--- InfoCar/app/src/main/java/com/example/infocar/AddCarActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/AddCarActivity.kt
@@ -2,11 +2,31 @@ package com.example.infocar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class AddCarActivity : AppCompatActivity() {
+
+    lateinit var brandRegister : EditText
+    lateinit var modelRegister : EditText
+    lateinit var vehicleYearRegister : EditText
+    lateinit var hpRegister : EditText
+    lateinit var yoursSinceRegister : EditText
+    lateinit var kmRegister : EditText
+    lateinit var registerNumberVehicle : EditText
+    lateinit var 
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_car)
+
+        brandRegister = findViewById(R.id.brandRegister)
+        modelRegister = findViewById(R.id.modelRegister)
+        vehicleYearRegister = findViewById(R.id.vehicleTypeRegister)
+        hpRegister = findViewById(R.id.hpRegister)
+        yoursSinceRegister = findViewById(R.id.yoursSinceRegister)
+        kmRegister = findViewById(R.id.kmRegister)
+        registerNumberVehicle = findViewById(R.id.registerNumberVehicle)
     }
 }
