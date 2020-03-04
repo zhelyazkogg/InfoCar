2648eeaefbc1f7305031d5b4def9696b5369f283
unknown <zhelyazkogg@gmail.com>
2020-03-04 18:29:36 +0200

    trying to add autocomplete in the AddCarActivity unseccessully

--- InfoCar/app/src/main/java/com/example/infocar/AddCarActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/AddCarActivity.kt
@@ -3,10 +3,7 @@ package com.example.infocar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Spinner
+import android.widget.*
 
 class AddCarActivity : AppCompatActivity() {
 
@@ -17,13 +14,10 @@ class AddCarActivity : AppCompatActivity() {
     lateinit var yoursSinceRegister : EditText
     lateinit var kmRegister : EditText
     lateinit var registerNumberVehicle : EditText
-    lateinit var vehicleTypeRegister : EditText
+    lateinit var vehicleTypeRegister : AutoCompleteTextView
     lateinit var fuelTypeRegister : EditText
     lateinit var buttonRegister : Button
     lateinit var buttonBack : Button
-/*    internal lateinit var vehicleTypeRegister : Spinner
-    lateinit var fuelTypeRegister : Spinner // TODO*/
-
     // TODO Buttons for Back and Confirm
 
 
@@ -34,19 +28,27 @@ class AddCarActivity : AppCompatActivity() {
 
         brandRegister = findViewById(R.id.brandRegister)
         modelRegister = findViewById(R.id.modelRegister)
-
         hpRegister = findViewById(R.id.hpRegister)
         yoursSinceRegister = findViewById(R.id.yoursSinceRegister)
         kmRegister = findViewById(R.id.kmRegister)
         registerNumberVehicle = findViewById(R.id.registerNumberVehicle)
-        vehicleTypeRegister = findViewById(R.id.vehicleTypeRegister)
+     //   vehicleTypeRegister = findViewById(R.id.vehicleTypeRegister)
         fuelTypeRegister = findViewById(R.id.fuelTypeRegister)
-        buttonRegister = findViewById(R.id.buttonRegister)
         buttonBack = findViewById(R.id.buttonBack)
+        buttonRegister = findViewById(R.id.buttonRegister)
 
-       /* vehicleTypeRegister = findViewById<Spinner>(R.id.vehicleTypeRegister)
-            val vehicleType = arrayOf("Sedan", "Coupe", "Cabriolet", "Hatchback", "Wagon", "Van", "SUV", "Other")
-            vehicleTypeRegister.adapter*/
+        val vehicleTypeRegister = findViewById<AutoCompleteTextView>(R.id.vehicleTypeRegister)
+        val vehicleTypeMenuRegister = resources.getStringArray(R.array.vehicleTypeMenu)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, vehicleTypeMenuRegister)
+        vehicleTypeRegister.setAdapter(adapter)
+
+        val button = findViewById<Button>(R.id.buttonRegister)
+        if (button != null) {
+            button setOnClickListener { v ->
+                val enteredText = getString(R.string.submitted_lang) + "" + vehicleTypeRegister.getText()
+                Toast.makeText(this@AddCarActivity,enteredText, Toast.LENGTH_SHORT).show()
+            }
+        }
 
         buttonBack.setOnClickListener {
             val LoginActivity = Intent(applicationContext, LoginActivity::class.java)
