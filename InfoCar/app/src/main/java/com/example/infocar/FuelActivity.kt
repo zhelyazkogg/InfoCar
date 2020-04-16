fd03a5c6b016d870466c617e6cca58001bee14d2
unknown <zhelyazkogg@gmail.com>
2020-04-16 14:15:51 +0300

    added the fuel Activity

---
+++ InfoCar/app/src/main/java/com/example/infocar/FuelActivity.kt
@@ -0,0 +1,13 @@
+package com.example.infocar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FuelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fuel)
+        
+    }
+}
