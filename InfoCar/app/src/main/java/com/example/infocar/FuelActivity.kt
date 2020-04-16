701fa0470491f81d84aa4f10b4f1bf1b5984c457
unknown <zhelyazkogg@gmail.com>
2020-04-16 14:16:12 +0300

    adjustments

--- InfoCar/app/src/main/java/com/example/infocar/FuelActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/FuelActivity.kt
@@ -8,6 +8,6 @@ class FuelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fuel)
-        
+
     }
 }
