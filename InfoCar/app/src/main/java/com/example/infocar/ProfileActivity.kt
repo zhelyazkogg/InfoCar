023575806d5b3400fe1e2ca2db0c8d6f5bd0a83a
unknown <zhelyazkogg@gmail.com>
2020-04-19 15:42:07 +0300

    creating the text and edit fields

--- InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
@@ -2,14 +2,24 @@ package com.example.infocar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 
-    class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity() {
 
     lateinit var mAuth : FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        val brandFirebase = findViewById<EditText>(R.id.brandFirebase)
+        val modelFirebase = findViewById<EditText>(R.id.modelFirebase)
+        val vehicleTypeFirebase = findViewById<EditText>(R.id.vehicleTypeFirebase)
+        val fuelFirebase = findViewById<EditText>(R.id.fuelFirebase)
+        val engineFirebase = findViewById<EditText>(R.id.engineFirebase)
+        val yoursSinceFirebase = findViewById<EditText>(R.id.yoursSinceFirebase)
+        val kmFirebase = findViewById<EditText>(R.id.kmFirebase)
+        val licensePlateFirebase = findViewById<EditText>(R.id.licensePlateFirebase)
     }
 }
