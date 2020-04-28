e4afed9a792c4e6e88485f3c6df96c95992a8491
unknown <zhelyazkogg@gmail.com>
2020-04-28 14:52:18 +0300

    added a logout button in the profile activity

--- InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
@@ -1,17 +1,24 @@
 package com.example.infocar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : AppCompatActivity() {
 
-    lateinit var mAuth : FirebaseAuth
+    private lateinit var mAuth : FirebaseAuth
+    private lateinit var mDatabase: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        mDatabase = FirebaseDatabase.getInstance().reference
 
         val brandFirebase = findViewById<EditText>(R.id.brandFirebase)
         val modelFirebase = findViewById<EditText>(R.id.modelFirebase)
@@ -21,5 +28,11 @@ class ProfileActivity : AppCompatActivity() {
         val yoursSinceFirebase = findViewById<EditText>(R.id.yoursSinceFirebase)
         val kmFirebase = findViewById<EditText>(R.id.kmFirebase)
         val licensePlateFirebase = findViewById<EditText>(R.id.licensePlateFirebase)
+
+        logoutButton.setOnClickListener{
+            val LoginActivity = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(LoginActivity)
+            finish()
+        }
     }
 }
