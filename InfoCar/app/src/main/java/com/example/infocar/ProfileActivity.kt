3a7dd7315ba0f704005bfc0a5f4149228a8e990d
unknown <zhelyazkogg@gmail.com>
2020-04-29 14:06:04 +0300

    added a logout button even tho it isn't working

--- InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/ProfileActivity.kt
@@ -3,13 +3,15 @@ package com.example.infocar
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_profile.*
+import kotlin.math.log
 
-class ProfileActivity : AppCompatActivity() {
+class ProfileActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var mAuth : FirebaseAuth
     private lateinit var mDatabase: DatabaseReference
@@ -19,6 +21,7 @@ class ProfileActivity : AppCompatActivity() {
         setContentView(R.layout.activity_profile)
 
         mDatabase = FirebaseDatabase.getInstance().reference
+        logoutButton.setOnClickListener(this)
 
         val brandFirebase = findViewById<EditText>(R.id.brandFirebase)
         val modelFirebase = findViewById<EditText>(R.id.modelFirebase)
@@ -33,6 +36,16 @@ class ProfileActivity : AppCompatActivity() {
             val LoginActivity = Intent(applicationContext, LoginActivity::class.java)
             startActivity(LoginActivity)
             finish()
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.logoutButton ->{
+                mAuth.signOut()
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()    
+            }
         }
     }
 }
