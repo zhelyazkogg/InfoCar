5bf4ae73f93e5a3b3ba45c7a3aca43b4a679e4d0
unknown <zhelyazkogg@gmail.com>
2020-05-08 14:26:50 +0300

    added a fun for the Expenses db

--- InfoCar/app/src/main/java/com/example/infocar/activities/utils.kt
+++ InfoCar/app/src/main/java/com/example/infocar/activities/utils.kt
@@ -3,6 +3,7 @@ package com.example.infocar.activities
 import android.content.Context
 import android.widget.Toast
 import com.example.infocar.models.CarInfo
+import com.example.infocar.models.Expenses
 import com.example.infocar.models.User
 import com.google.firebase.database.DataSnapshot
 
@@ -13,5 +14,8 @@ fun Context.showToast(text: String, duration: Int = Toast.LENGTH_SHORT) {
 fun DataSnapshot.asUser(): User? =
     getValue(User::class.java)?.copy(uid = key)
 
-fun DataSnapshot.carInfo(): CarInfo? =
+fun DataSnapshot.uCarInfo(): CarInfo? =
     getValue(CarInfo::class.java)?.copy(uid = key)
+
+fun DataSnapshot.uExpenses(): Expenses =
+    getValue(Expenses::class.java)?.copy(uid = key)
