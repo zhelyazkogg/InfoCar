34dfe733d86dbb3c3583e31e191fbfb7c3260c81
Zhelyazko <zhelyazkogg@gmail.com>
2020-09-08 14:27:22 +0300

    added another expense

--- InfoCar/app/src/main/java/com/example/infocar/models/Reminders.kt
+++ InfoCar/app/src/main/java/com/example/infocar/models/Reminders.kt
@@ -1,13 +1,13 @@
 package com.example.infocar.models
 
-import com.google.firebase.database.Exclude
 import com.google.firebase.database.IgnoreExtraProperties
 
 @IgnoreExtraProperties
 data class Reminders (
     val carLiabilityInsurance: String="", //граждаска отговорност
     val carHullInsurance: String="", // автокаско на автомобила
-    val carInspection: String="" // преглед на автомобила
+    val carInspection: String="", // преглед на автомобила
+    val carTax: String="" // данък на автомобила
 //@Exclude val uid: String=""
 
 )
