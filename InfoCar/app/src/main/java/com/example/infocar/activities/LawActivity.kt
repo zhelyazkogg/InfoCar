4c1fc4d9b6a87500ab88ec45af4f9c3408699bfe
Zhelyazko <zhelyazkogg@gmail.com>
2020-09-20 20:10:51 +0300

    modified

--- InfoCar/app/src/main/java/com/example/infocar/activities/LawActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/activities/LawActivity.kt
@@ -1,13 +1,26 @@
 package com.example.infocar.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.infocar.R
+import kotlinx.android.synthetic.main.activity_law.*
 
-class LawActivity : AppCompatActivity() {
+class LawActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_law)
+
+        buttonBack.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.buttonBack -> {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }
     }
 }
