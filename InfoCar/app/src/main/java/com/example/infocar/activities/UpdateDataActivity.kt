e73be8483014725836a87b7fa9c3a5cc695b6d0c
Zhelyazko <zhelyazkogg@gmail.com>
2020-09-21 11:35:53 +0300

    Updating the updateDataActivity

--- InfoCar/app/src/main/java/com/example/infocar/activities/UpdateDataActivity.kt
+++ InfoCar/app/src/main/java/com/example/infocar/activities/UpdateDataActivity.kt
@@ -5,16 +5,38 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.infocar.R
+import com.example.infocar.models.User
+import com.example.infocar.models.UserCarInfo
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_update_data.*
 
 class UpdateDataActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var mDatabase: DatabaseReference
+    private lateinit var mUserCarInfo: UserCarInfo
+    private lateinit var mUser: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_update_data)
 
+        mAuth = FirebaseAuth.getInstance()
+        mDatabase = FirebaseDatabase.getInstance().reference
+        buttonUpdate.setOnClickListener(this)
+        buttonBack.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.buttonUpdate -> {
+//                update()
+            }
+        }
+
         when (v?.id) {
             R.id.buttonBack -> {
                 startActivity(Intent(this, ProfileActivity::class.java))
